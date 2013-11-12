bba6bac059d00d14ee4aafbf154b649911343d45
Lukas Ocilka <lukas.ocilka@gmail.com>
2013-11-12 14:57:24 +0100

    A bit more descriptive tests

--- test/nfs_options_test.rb
+++ test/nfs_options_test.rb
@@ -7,7 +7,7 @@ require "yast"
 Yast.import "NfsOptions"
 
 describe "#validate" do
-  it "returns empty message on correct options" do
+  it "returns empty string on correct options" do
     [
       "defaults",
       "nolock,bg",
@@ -20,43 +20,43 @@ describe "#validate" do
     end
   end
 
-  it "returns error message on incorrect options" do
-    [
-      "",
-      "nolock, bg",
-      "nolock,unknownoption",
-      "nolock,unknownassignment=true",
-      "nolock,rsize=",
-      "nolock,two=equal=signs",
-      "nolock,retrans=trans=trans",
-      "nolock,intr=bogus",
-    ].each do |options|
-      returned = Yast::NfsOptions.validate(options)
-      expect(returned).not_to be_empty, "options '#{options}' returned '#{returned}'"
-    end
+  it "returns 'Empty option strings are not allowed' error message on empty options" do
+    returned = Yast::NfsOptions.validate("")
+    expect(returned).to start_with("Empty option strings are not allowed"), "options '' returned '#{returned}'"
   end
 
-  it "returns error message on incorrect options" do
+  it "returns 'Empty value' error message on incorrect options" do
     [
       "noatime,port=",
       "mountvers=",
       "mountvers,=port=23",
+      "nolock,rsize=",
     ].each do |options|
       returned = Yast::NfsOptions.validate(options)
       expect(returned).to start_with("Empty value"), "options '#{options}' returned '#{returned}'"
     end
+  end
 
+  it "returns 'Invalid option' error message on incorrect options" do
     [
       "noatime,port=dort=fort",
       "mountvers=port=23",
+      "nolock,retrans=trans=trans",
     ].each do |options|
       returned = Yast::NfsOptions.validate(options)
       expect(returned).to start_with("Invalid option"), "options '#{options}' returned '#{returned}'"
     end
+  end
 
+  it "returns 'Unknown option' error message on incorrect options" do
     [
       "noatime,unknownparam",
       "mountvers2",
+      "nolock, bg",
+      "nolock,unknownoption",
+      "nolock,unknownassignment=true",
+      "nolock,intr=bogus",
+      "nolock,two=equal=signs",
     ].each do |options|
       returned = Yast::NfsOptions.validate(options)
       expect(returned).to start_with("Unknown option"), "options '#{options}' returned '#{returned}'"
