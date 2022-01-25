yast/yast-nfs-client

commit 4196cbd73f578a07228ba680f4a0278c08047b5e

Update yast2 dependency

package/yast2-nfs-client.spec
@@ -28,15 +28,15 @@ BuildRequires:  perl-XML-Writer
 BuildRequires:  update-desktop-files
 BuildRequires:  yast2-devtools >= 3.1.27
 BuildRequires:  yast2-testsuite
-# SuSEFirewall2 replaced by firewalld (fate#323460)
-BuildRequires:  yast2 >= 4.0.39
+# Yast::Execute.locally
+BuildRequires:  yast2 >= 4.1.42
 BuildRequires:  rubygem(rspec)
 #for install task
 BuildRequires:  rubygem(%rb_default_ruby_abi:yast-rake)
 # path_matching (RSpec argument matcher)
 BuildRequires:  yast2-ruby-bindings >= 3.1.31
-# SuSEFirewall2 replaced by firewalld (fate#323460)
-Requires:       yast2 >= 4.0.39
+# Yast::Execute.locally
+BuildRequires:  yast2 >= 4.1.42
 #idmapd_conf agent
 Requires:       yast2-nfs-common >= 2.24.0
 # showmount, #150382, #286300

commit 5dd79aea925520279dc2bd3575f90a33d97a87db

Update dependency on the yast2 package
* It fixes a dependency problem introduced in 4196cbd.

package/yast2-nfs-client.spec
@@ -30,8 +30,8 @@ BuildRequires:  perl-XML-Writer
 BuildRequires:  update-desktop-files
 BuildRequires:  yast2-devtools >= 4.2.2
 BuildRequires:  yast2-testsuite
-# Yast::Execute.locally
-BuildRequires:  yast2 >= 4.1.42
+# Replace PackageSystem with Package
+BuildRequires:  yast2 >= 4.4.38
 BuildRequires:  rubygem(%{rb_default_ruby_abi}:rspec)
 #for install task
 BuildRequires:  rubygem(%rb_default_ruby_abi:yast-rake)
@@ -46,8 +46,8 @@ BuildRequires:  yast2-storage-ng >= 4.2.73
 BuildRequires: rubygem(%{rb_default_ruby_abi}:yast-rake-ci)
 %endif
 
-# Yast::Execute.locally
-BuildRequires:  yast2 >= 4.1.42
+# Replace PackageSystem with Package
+Requires:       yast2 >= 4.4.38
 #idmapd_conf agent
 Requires:       yast2-nfs-common >= 2.24.0
 # showmount, #150382, #286300